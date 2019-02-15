78b2b81dd525b3cc1baba2d516a5ee4639923be1
PabloSz <pablosaez1995@gmail.com>
2019-02-15 01:07:55 -0300

    building shell

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const HashFolder = require("./lib/HashFolder");
 const Checksum = require("./lib/Checksum");
-
+const Shell = require("./lib/Shell");
 module.exports = class createStaticBuild {
-  constructor({ context, folder, name } = {}) {
+  constructor({ context, folder, name, adjust, staticPath } = {}) {
     this.context = context;
     this.folder = folder;
     this.name = name;
+    this.adjust = adjust;
 
     this.hashFolder = new HashFolder({ context, folder, name });
     this.checksum = new Checksum({ context, folder, name });
-
+    this.shell = new Shell({ context, adjust, name, staticPath, folder });
     this.check();
   }
 
-  async check() {
+  async check(name = this.name) {
     try {
       const hash = await this.hashFolder.getHashFolder();
       await this.checksum.ready();
-      this.checksum.checkHashFolder(hash);
+      const build = await this.checksum.checkHashFolder(hash);
+      if (build) {
+        console.log(`${name} building...`);
+        this.shell.build();
+      } else {
+        console.log(`No changes detected in ${name}`);
+      }
     } catch (err) {
       console.error(err);
     }
