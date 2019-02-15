7d9f935c1319d9f7f365959793f20fa8d9876002
PabloSz <pablosaez1995@gmail.com>
2019-02-14 22:22:10 -0300

    checksum and hashfolder implemented

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  console.log("todo ok");
+const HashFolder = require("./lib/HashFolder");
+const Checksum = require("./lib/Checksum");
+
+module.exports = class createStaticBuild {
+  constructor({ context, folder, name } = {}) {
+    this.context = context;
+    this.folder = folder;
+    this.name = name;
+
+    this.hashFolder = new HashFolder({ context, folder, name });
+    this.checksum = new Checksum({ context, folder, name });
+
+    this.check();
+  }
+
+  async check() {
+    try {
+      const hash = await this.hashFolder.getHashFolder();
+      await this.checksum.ready();
+      this.checksum.checkHashFolder(hash);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 };
